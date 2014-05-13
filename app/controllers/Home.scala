0be2ed77bb231c52b48a01005b2379bbf019e14e
Tomofumi Tanaka <tanacasino@gmail.com>
2014-05-14 01:11:08 +0900

    Add logging

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -1,12 +1,19 @@
 package controllers
 
 import play.api.mvc._
+import play.api.Logger
 
 object Home extends Controller {
 
   def index = Action { implicit request =>
     val userAgent = request.headers.get("User-Agent")
     val remoteAddr = request.remoteAddress
+    Logger.debug(s"X-Forwarded-For: ${request.headers.get("X-Forwarded-For")}")
+    val xff = request.headers.get("X-Forwarded-For") match {
+      case Some(x) => x
+      case None => request.remoteAddress
+    }
+    Logger.debug(s"xff : $xff")
     val env = Environment(userAgent.getOrElse("None"), remoteAddr, request.headers.toString)
     Ok(views.html.home(env))
   }
